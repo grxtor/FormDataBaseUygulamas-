863bbe99034bf5e3ce40fdebb1ffb877fce467a8
GRXTOR <86706456+grxtor@users.noreply.github.com>
2024-03-25 21:20:25 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp15
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.a = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.a);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(436, 151);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DataBase";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(435, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tabloya Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // a
+            // 
+            this.a.AutoSize = true;
+            this.a.Location = new System.Drawing.Point(6, 89);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(37, 13);
+            this.a.TabIndex = 2;
+            this.a.Text = "Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "AD";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(435, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Tabloyu Göster";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(454, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(368, 145);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(454, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(368, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Tablodan Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 253);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Öğrenci Ekle/Çıkar Uygulaması";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label a;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            VerileriYenile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+
+                string connectionString = @"Data Source=DESKTOP-6DMTNPM;Initial Catalog=a;Integrated Security=True";
+                SqlConnection baglanti = new SqlConnection(connectionString);
+
+                try
+                {
+                    baglanti.Open();
+                    SqlCommand komut = new SqlCommand("INSERT INTO [dbo].[Table] ([Soyad], [ad]) VALUES (@Soyad,@ad)", baglanti);
+
+                    komut.Parameters.AddWithValue("@Soyad", textBox2.Text);
+                    komut.Parameters.AddWithValue("@ad", textBox1.Text);
+
+                    komut.ExecuteNonQuery();
+                    MessageBox.Show("Kayıt yapıldı");
+                    VerileriYenile();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Bir hata oluştu: " + ex.Message);
+                }
+                finally
+                {
+                    baglanti.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("TextBox'lar boş olamaz");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=DESKTOP-6DMTNPM;Initial Catalog=a;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                string sqlQuery = "SELECT * FROM [dbo].[Table]";
+
+                try
+                {
+                    connection.Open();
+
+                    SqlDataAdapter da = new SqlDataAdapter(sqlQuery, connection);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Bir hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var selectedRow = dataGridView1.SelectedRows[0];
+            if (dataGridView1.SelectedRows.Count > 0 || dataGridView1.SelectedRows[0].Cells == null || selectedRow.Cells["Id"].Value == DBNull.Value)
+            {
+                
+                int selectedRowId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+
+                string connectionString = @"Data Source=DESKTOP-6DMTNPM;Initial Catalog=a;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    string sqlQuery = "DELETE FROM [dbo].[Table] WHERE Id = @Id";
+
+                    try
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand(sqlQuery, connection);
+                        // Parametreyi güvenli bir şekilde ekleyin
+                        command.Parameters.AddWithValue("@Id", selectedRowId);
+
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Kayıt silindi.");
+
+                        VerileriYenile();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Bir hata oluştu: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silinecek bir kayıt seçin.");
+            }
+        }
+
+        private void VerileriYenile()
+        {
+            string connectionString = @"Data Source=DESKTOP-6DMTNPM;Initial Catalog=a;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    // SqlDataAdapter ve DataTable kullanarak veri çekme
+                    SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM [dbo].[Table]", connection);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    // DataGridView kontrolünde verileri gösterme
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Bir hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
+
+    }
+}
